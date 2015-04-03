c05790e7e18450e9740a7f586a1b050b7c33e614
Eric Meyer <emeyer@8thlight.com>
2015-04-03 17:56:43 -0500

    Implement an endpoint for "add_match"

--- server.rb
+++ server.rb
@@ -1,7 +1,33 @@
 require 'sinatra'
 require 'json'
 
+class Queue
+
+  @matches = []
+
+  def self.all
+    @matches
+  end
+
+  def self.add_match(options)
+    @matches << options.merge({
+      id: rand(100000).to_s
+    })
+  end
+
+end
+
+post '/add_match' do
+  Queue.add_match({
+    playerOne: params[:playerOne].to_s,
+    playerTwo: params[:playerTwo].to_s
+  })
+  {
+    success: true
+  }.to_json
+end
+
 get '/current_queue' do
   content_type "application/json"
-  File.read("./responses/current_queue.json")
+  Queue.all.to_json
 end
